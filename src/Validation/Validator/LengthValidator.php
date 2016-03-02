34ab5803a6cb43f429d0493a40dac4ebc404747e
Grigorii Duca <contact@greg.md>
2016-03-02 18:13:40 +0200

    .

---
+++ src/Validation/Validator/LengthValidator.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Greg\Validation\Validator;
+
+use Greg\Tool\Obj;
+use Greg\Validation\ValidatorInterface;
+use Greg\Validation\ValidatorTrait;
+
+class LengthValidator implements ValidatorInterface
+{
+    use ValidatorTrait;
+
+    protected $length = null;
+
+    public function __construct($length)
+    {
+        $this->length($length);
+
+        return $this;
+    }
+
+    public function validate($value, array $values = [])
+    {
+        $errors = [];
+
+        $length = $this->length();
+
+        if (mb_strlen($value) != $length) {
+            $errors[] = 'Value length should be ' . $length . '.';
+        }
+
+        if ($errors) {
+            $this->errors($errors, true);
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public function length($value = null)
+    {
+        return Obj::fetchIntVar($this, $this->{__FUNCTION__}, true, ...func_get_args());
+    }
+}
